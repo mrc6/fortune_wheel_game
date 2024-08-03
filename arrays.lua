c5cf713eff3d47b433a8ce9d74deebfd8d5a4090
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2024-08-03 19:23:02 -0300

    Add files via upload

---
+++ arrays.lua
@@ -0,0 +1,11 @@
+local arrays = {
+  indexOf = function(array, value)
+    for i, v in ipairs(array) do
+        if v == value then
+            return i
+        end
+    end
+    return nil
+  end
+}
+return arrays
